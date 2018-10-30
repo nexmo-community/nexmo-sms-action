9dae5aa5ee565a770067752a2df9295e2806ff00
Steve Crow <steve.crow@vonage.com>
2018-10-30 17:42:58 -0400

    Pipe cli output to dev null.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-sh -c "nexmo setup ${NEXMO_API_KEY} ${NEXMO_API_SECRET}"
-sh -c "nexmo sms --from=${NEXMO_NUMBER} ${TO_NUMBER} $* --confirm"
+sh -c "nexmo setup ${NEXMO_API_KEY} ${NEXMO_API_SECRET} > /dev/null"
+sh -c "nexmo sms --from=${NEXMO_NUMBER} ${TO_NUMBER} $* --confirm > /dev/null"
